67b16c81bbd0e1939c5e08c1b865f7827f9a42f4
deep5050 <dipankarpal5050@gmail.com>
2020-05-28 16:52:06 +0530

    more badges added

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
+#!/usr/bin/env node
+'use strict'
+
 const qoa = require('qoa')
 const Conf = require('conf');
 const fs = require('fs');
 const config = new Conf();
 
 config.clear();
-
+//console.log(config.path);
 const shields = "https://img.shields.io";
 const badgen = "https://badgen.net";
 
-const table_header = `| Syntax    |    Badge  |\n| :---      |  :----:  |`;
+const table_header = `\n| Syntax    |    Badge  |\n| :---      |  :----:  |`;
 
 var get_badge_text = (provider, path, url, alt) => {
     var type = "";
     var style = "";
+   
     if (config.has('style')) {
         style = config.get('style');
     }
@@ -21,267 +25,289 @@ var get_badge_text = (provider, path, url, alt) => {
     }
 
     if (type === 'HTML') {
-        return text = `<img alt="${alt}" src="${provider}${path}?style=${style}"></img>`;
+        return `<img alt="${alt}" src="${provider}${path}style=${style}"></img>`;
     }
     else if (type === 'MarkDown') {
-        return text = `[![${alt}](${provider}${path}?style=${style})](${url})`;
+        return `[![${alt}](${provider}${path}style=${style})](${url})`;
     }
 }
 
 
 
-
-
-
-
-
-
-
-
-var create_column = (val) => {
-    return text = "\n| `" + val + "` | " + val + "|";
-
+var make_a_row = (val) => {
+    return  "\n| `" + val + "` | " + val + "|";
 }
 
 
 
-function rating_bages() { };
+var rating_bages = () => { }
 
-function other_badges() { };
+var other_badges = (user, repo) => {
+    var text = "\n## Other Badges\n"
+    text += table_header;
+    var url = `https://github.com/${user}/${repo}`;
+    var hit_count = get_badge_text(
+        "http://hits.dwyl.com",
+        `/${user}/${repo}.svg?`,
+        url,
+        'Hit Count'
+    );
+    text += make_a_row(hit_count);
 
-//-----------------------------------------
+    return text;
+}
 
-function social_badges(user, repo) {
+
+var social_badges = (user, repo) => {
     var text = "\n## Social Badges\n"
     text += table_header;
     var url = `https://github.com/${user}/${repo}`;
 
     var followers = get_badge_text(
         shields,
-        `/github/followers/${user}`,
+        `/github/followers/${user}?`,
         url,
         'Followers'
     );
-    //console.log(followers);
-    text += create_column(followers);
+ 
+    text += make_a_row(followers);
 
     var forks = get_badge_text(
         shields,
-        `/github/forks/${user}/${repo}`,
+        `/github/forks/${user}/${repo}?`,
         url,
         'Forks'
     );
 
     //console.log(forks);
-    text += create_column(forks);
+    text += make_a_row(forks);
 
 
     var stars = get_badge_text(
         shields,
-        `/github/stars/${user}/${repo}`,
+        `/github/stars/${user}/${repo}?`,
         url,
         'Stars'
     );
     // console.log(stars);
-    text += create_column(stars);
+    text += make_a_row(stars);
 
 
     var watchers = get_badge_text(
         shields,
-        `/github/watchers/${user}/${repo}`,
+        `/github/watchers/${user}/${repo}?`,
         url,
         'Watchers'
     );
     //console.log(watchers);
-    text += create_column(watchers);
+    text += make_a_row(watchers);
 
 
     if (config.has('twitter')) {
         if (config.get('twitter') !== false && config.get('twitter.id') !== undefined) {
             var twitter_follow = get_badge_text(
                 shields,
-                `/twitter/follow/${config.get('twitter.id')}?logo=twitter`,
+                `/twitter/follow/${config.get('twitter.id')}?logo=twitter&`,
                 url,
                 'Twitter Follow'
             );
             // console.log(twitter_follow);
-            text += create_column(twitter_follow);
+            text += make_a_row(twitter_follow);
         }
     }
     return text;
 }
 
-//-----------------------------------------
-
-function dependency_badges(user, repo) {
+var dependency_badges = (user, repo) => {
 
 }
 
 
-//------------------------------------------
-function version_badges(user, repo) {
+var version_badges = (user, repo) => {
     var text = "\n## Version Badges\n";
     text += table_header;
     var url = `https://github.com/${user}/${repo}`;
 
     var version = get_badge_text(
         shields,
-        `/github/v/release/${user}/${repo}`,
+        `/github/v/release/${user}/${repo}?`,
         url,
         'Release Version'
     );
     //console.log(version);
-    text += create_column(version);
+    text += make_a_row(version);
     return text;
 }
 
 
+var ci_cd_badges = (user, repo) => {
+    var text = "\n## CI/CD Badges\n";
+    text += table_header;
+    var url = `https://github.com/${user}/${repo}`;
 
-//-------------------------------------------
-function activity_badges(user, repo) {
+    if (config.get('github') === false || !config.has('github')) {
+        return text = "\n## CI/CD Badges\n";
+    }
+    var github_workflow = get_badge_text(
+        shields,
+        `/github/workflow/status/${user}/${repo}/${config.get('github.workflow')}?label=${config.get('github.workflow')}&logo=github&`,
+        url,
+        'GitHub Workflow Status'
+    );
+
+    text += make_a_row(github_workflow);
+
+    if (config.has('ci_cd_provider') && config.get('ci_cd_provider') === 'Travis') {
+        var travis = get_badge_text(
+            shields,
+            `/travis/${user}/${repo}?label=Build&logo=travis&`,
+            url,
+            'Travis Build'
+        );
+        text += make_a_row(travis);
+    }
+
+    return text;
+}
+
+
+var activity_badges = (user, repo) => {
     var text = "\n## Activity Badges\n";
     text += table_header;
     var url = `https://github.com/${user}/${repo}`;
 
     var commits_per_month = get_badge_text(
         shields,
-        `/github/commit-activity/m/${user}/${repo}`,
+        `/github/commit-activity/m/${user}/${repo}?`,
         url,
         'Commits/month'
     );
     //console.log(commits_per_month);
-    text += create_column(commits_per_month);
+    text += make_a_row(commits_per_month);
 
 
     var last_commit = get_badge_text(
         shields,
-        `/github/last-commit/${user}/${repo}`,
+        `/github/last-commit/${user}/${repo}?`,
         url,
         'Last Commit'
     );
     // console.log(last_commit);
-    text += create_column(last_commit);
+    text += make_a_row(last_commit);
 
 
     var relese_date = get_badge_text(
         shields,
-        `/github/release-date/${user}/${repo}`,
+        `/github/release-date/${user}/${repo}?`,
         url,
         'Last release date'
     );
     // console.log(relese_date);
-    text += create_column(relese_date);
+    text += make_a_row(relese_date);
 
 
     var contributors = get_badge_text(
         shields,
-        `/github/contributors/${user}/${repo}`,
+        `/github/contributors/${user}/${repo}?`,
         url,
         'Contributors'
     );
     // console.log(contributors);
-    text += create_column(contributors);
+    text += make_a_row(contributors);
 
     return text;
 }
 
-
-//--------------------------------------------
-
-function license_badges(user, repo) {
-    var text = "\n## License Badges";
+var license_badges = (user, repo) => {
+    var text = "\n## License Badges\n";
     text += table_header;
     var url = `https://github.com/${user}/${repo}`;
     var license = get_badge_text(
         shields,
-        `/github/license/${user}/${repo}`,
+        `/github/license/${user}/${repo}?`,
         url,
         'License'
     );
     // console.log(license);
-    text += create_column(license);
+    text += make_a_row(license);
     return text;
 }
 
 
-
-//----------------------------------------------------
-
-function size_badges(user, repo) {
+var size_badges = (user, repo) => {
     var text = "\n## Size Badges\n";
     text += table_header;
 
     var url = `https://github.com/${user}/${repo}`;
-    var repo_size = get_badge_text(shields, `/github/repo-size/${user}/${repo}`, url, 'Repo Size');
+    var repo_size = get_badge_text(shields, `/github/repo-size/${user}/${repo}?`
+    , url, 'Repo Size');
     // console.log(repo_size);
-    text += create_column(repo_size);
+    text += make_a_row(repo_size);
     return text;
 }
 
 
-//----------------------------------------------------
-
-function analysis_badges(user, repo) {
+var analysis_badges = (user, repo) => {
     var text = "\n## Analysis Badges\n";
     text += table_header;
     var url = `https://github.com/${user}/${repo}`;
 
     var languages_count = get_badge_text(
         shields,
-        `/github/languages/count/${user}/${repo}`,
+        `/github/languages/count/${user}/${repo}?`,
         url,
         'Language Count'
     );
     // console.log(languages_count);
-    text += create_column(languages_count);
+    text += make_a_row(languages_count);
 
     var top_language = get_badge_text(
         shields,
-        `/github/languages/top/${user}/${repo}`,
+        `/github/languages/top/${user}/${repo}?`,
         url,
         'Top Language'
     );
     // console.log(top_language);
-    text += create_column(top_language);
+    text += make_a_row(top_language);
 
     if (config.has('analysis_provider') && config.get('analysis_provider') === 'CodeClimate') {
         var codeclimate_maintain_percent = get_badge_text(
             shields,
-            `/codeclimate/maintainability-percentage/${user}/${repo}`,
+            `/codeclimate/maintainability-percentage/${user}/${repo}?`,
             url,
             'Code Climate maintainability'
         );
         // console.log(codeclimate_maintain_percent);
-        text += create_column(codeclimate_maintain_percent);
+        text += make_a_row(codeclimate_maintain_percent);
 
         var codeclimate_issues = get_badge_text(
             shields,
-            `/codeclimate/issues/${user}/${repo}`,
+            `/codeclimate/issues/${user}/${repo}?`,
             url,
             "Code Climate Issues"
         );
         // console.log(codeclimate_issues);
-        text += create_column(codeclimate_issues);
+        text += make_a_row(codeclimate_issues);
 
     }
     if (config.has('analysis_provider') && config.get('analysis_provider') === 'Codacy') {
         if (config.has('codacy.project_id') && config.get('codacy.project_id') !== "") {
             var codacy_project_id = config.get('codacy.project_id');
             var codacy_grade = get_badge_text(
-                shields, `/codacy/grade/${codacy_project_id}`,
+                shields,
+                 `/codacy/grade/${codacy_project_id}?`,
                 url, 'Codacy Grade'
             );
             // console.log(codacy_grade);
-            text += create_column(codacy_grade);
+            text += make_a_row(codacy_grade);
         }
     }
     return text;
 }
 
 
-//------------------------------------------
 
-function issues_badges(user, repo) {
+var issues_badges = (user, repo) => {
     var text = "\n## Issues Badges\n";
     text += table_header;
     var url = `https://github.com/${user}/${repo}`;
@@ -289,67 +315,71 @@ function issues_badges(user, repo) {
 
     var issue_raw = get_badge_text(
         shields,
-        `/github/issues-raw/${user}/${repo}`,
+        `/github/issues-raw/${user}/${repo}?`,
         url,
         "Github Isuues"
     );
     // console.log(issues_raw);
-    text += create_column(issue_raw);
+    text += make_a_row(issue_raw);
 
 
 
     var issues_closed = get_badge_text(
         shields,
-        `/github/issues-closed/${user}/${repo}`,
+        `/github/issues-closed/${user}/${repo}?`,
         url,
         "Github closed Isuues"
     );
     // console.log(issues_closed);
-    text += create_column(issues_closed);
+    text += make_a_row(issues_closed);
 
 
 
     var pr_raw = get_badge_text(
         shields,
-        `/github/issues-pr-raw/${user}/${repo}`,
+        `/github/issues-pr-raw/${user}/${repo}?`,
         url,
         "Github open PRs"
     );
     // console.log(pr_raw);
-    text += create_column(pr_raw);
+    text += make_a_row(pr_raw);
 
 
 
     var pr_closed = get_badge_text(
         shields,
-        `/github/issues-pr-closed/${user}/${repo}`,
+        `/github/issues-pr-closed/${user}/${repo}?`,
         url,
         "Github closed PRs"
     );
     // console.log(pr_closed);
-    text += create_column(pr_closed);
+    text += make_a_row(pr_closed);
 
     return text;
 }
 
 
-//---------------------------------------------------------
+
 var analyze_inputs = () => {
 
-    const write_stream = fs.createWriteStream(`./autobadge-${config.get('style')}.md`);
+    const write_stream = fs.createWriteStream(`./autobadge-${config.get('repo')}-${config.get('style')}.md`);
     write_stream.write("# Autobadge\nThis is an auto-generated file\n");
 
     const user = config.get('user');
     const repo = config.get('repo');
 
 
+    write_stream.write(license_badges(user, repo));
     write_stream.write(social_badges(user, repo));
     write_stream.write(size_badges(user, repo));
+    write_stream.write(issues_badges(user, repo));
     write_stream.write(analysis_badges(user, repo));
     write_stream.write(activity_badges(user, repo));
     write_stream.write(version_badges(user, repo));
-    console.log("\x1b[32m","Badges Creation Completed");
+    write_stream.write(ci_cd_badges(user, repo));
+    write_stream.write(other_badges(user, repo));
 
+    console.log("\x1b[32m", "\nBadges Creation Completed :)");
 }
 
 
@@ -463,10 +493,27 @@ const interactive_mode = async () => {
         });
         config.set('codacy', project_id);
     }
+
+
+    var github_workflow = await qoa.confirm({
+        query: "GitHub Workflow added?",
+        handle: "github",
+        accept: 'y',
+        deny: 'n'
+    });
+    config.set(github_workflow);
+
+    if (config.has('github') && config.get('github') === true) {
+        var workflow_name = await qoa.input({
+            query: "Enter GitHub Workflow name:",
+            handle: "workflow",
+        });
+        config.set('github', workflow_name);
+    }
 }
 
 
 
 
 interactive_mode().then(analyze_inputs);
-// analyze_inputs();
+//analyze_inputs();
